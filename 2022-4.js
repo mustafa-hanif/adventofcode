35ac3f5a4a99b5e50682522cd6e145cb2e007b8c
Mustafa Hanif <mustafa@eventmobi.com>
2022-12-04 09:46:05 +0400

    added 2022-4

--- 2022-4.js
+++ 2022-4.js
@@ -1,26 +1,30 @@
 var fs = require('fs');
 
-const file = fs.readFileSync('input2022-4.txt', 'utf8');
+const file = fs.readFileSync('input2022-4.txt', 'utf8').trimEnd();
 let lines = file.split('\n');
 
 let count = 0;
 let dd = 0;
 for (let i = 0; i < lines.length; i++) {
-  const r1l = lines[i].split(',')[0].split('-')[0];
-  const r1h = lines[i].split(',')[0].split('-')[1];
+  const r1l = parseInt(lines[i].split(',')[0].split('-')[0], 10);
+  const r1h = parseInt(lines[i].split(',')[0].split('-')[1], 10);
 
-  const r2l = lines[i].split(',')[1].split('-')[0];
-  const r2h = lines[i].split(',')[1].split('-')[1];
+  const r2l = parseInt(lines[i].split(',')[1].split('-')[0], 10);
+  const r2h = parseInt(lines[i].split(',')[1].split('-')[1], 10);
   
-  if (r1l >= r2l && r1h <= r2h || r2l >= r1l && r2h <= r1h) {
-    console.log('overlap', r1l, r1h, r2l, r2h);
+  // part 1
+  // if ((r1l >= r2l && r1h <= r2h) || (r2l >= r1l && r2h <= r1h)) {
+  //   count++;
+  // } else {
+  //   dd++;
+  // }
+
+  // part 2
+  if ((r1l >= r2l && r1l <= r2h) || (r2l >= r1l && r2l <= r1h)) {
     count++;
-  } else {
-    console.log('no overlap', r1l, r1h, r2l, r2h);
-    dd++;
   }
 }
 
-console.log(count, dd);
+console.log(count);
 
 

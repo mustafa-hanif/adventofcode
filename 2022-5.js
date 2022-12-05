a0d2a2731a9ea98ac97106d93ef2a003d4a1be42
Mustafa Hanif <mustafa@eventmobi.com>
2022-12-05 12:52:56 +0400

    clean up

--- 2022-5.js
+++ 2022-5.js
@@ -3,22 +3,22 @@ var fs = require('fs');
 const file = fs.readFileSync('input2022-5.txt', 'utf8').trimEnd();
 let lines = file.split('\n');
 
-let count = 0;
-let dd = 0;
-
 const myitems = [];
 let cols = [];
 for (let i = 0; i < lines.length; i++) {
+  // the line which indexes the columns
   const numberLine = lines[i].match(/(\d+)/g);
+
+  // if its a line with items
   if (lines[i].split('').findIndex((x) => x === '[') > -1) {
     const items = lines[i].matchAll(/\[/g)
     for (const item of items) {
+      // record the item and the column it appeared in
       myitems.push({
         char: lines[i].split('')[item.index + 1],
         col: (item.index / 4) + 1,
       });
     }
-    // console.log(myitems);
   } else if (lines[i].split('move').length === 2) {
     const match = lines[i].match(/move (\d+) from (\d+) to (\d+)/);
     const count = parseInt(match[1], 10);
@@ -31,16 +31,14 @@ for (let i = 0; i < lines.length; i++) {
     // }
 
     // part 2
-    // console.log(cols[from].slice(-count))
-    // console.log(to, from, count);
-    
     cols[to] = cols[to].concat(cols[from].slice(-count));
     cols[from] = cols[from].slice(0, cols[from].length - count)
+
   } else if (numberLine?.length > 0) {
-    // console.log(numberLine[numberLine.length - 1]);
+    // once I am on the index line, I can start to build the columns
+    // 2d array of columns
     cols = new Array(+numberLine[numberLine.length - 1] + 1);
     [...myitems.reverse()].forEach(item => {
-      // console.log("pushing", item.char, "to", item.col);
       if (cols[item.col] === undefined) {
         cols[item.col] = [];
       }
@@ -48,7 +46,7 @@ for (let i = 0; i < lines.length; i++) {
     });
   }
 }
-// console.log(cols);
+// print the first item in each column
 console.log(cols.map(col => col[col.length-1]).join(''));
 
 
